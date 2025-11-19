b327c977d20dea629009e4caba4a48f1a88f0dc5
nacochuu <nacochuu@gmail.com>
2025-11-19 20:18:36 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -450,7 +450,7 @@ function setHeader() {
 
   const headerPc = /* HTML */ `
     <div class="pc w100p h164"></div>
-    <div class="pc header fbc w100p h164 bgFFF fixed top0 z200">
+    <div class="pc header fbc w100p h164 bgF9E8ED fixed top0 z200">
       <a href="./" class="fcc ml32">
         <img src="img/logo1.webp" class="w374" alt="" />
       </a>
@@ -532,8 +532,8 @@ function setFooter() {
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3485.1685800270866!2d131.43501605769313!3d34.16019916001695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x354490bfdd5cd357%3A0xb3984796b1053fee!2z44GK44GG44Gh44OZ44O844Kr44Oq44O8IHN0cmF3YmVycnl0aW1l!5e0!3m2!1sja!2sjp!4v1752224075332!5m2!1sja!2sjp";
   const footer = qs(".footer");
   footer.outerHTML = /* HTML */ `
-    <div class="pc footer fcc z100">
-      <div class="fxc abs bot155">
+    <div class="pc  w100p footer fcc z100 bgF9E8ED py80">
+      <div class="fxc">
         <iframe class="w440 h360" src="${mapurl}" loading="lazy"></iframe>
         <div class="v f  ml115">
           <img src="img/logo1.webp" class="w374" alt="" />
@@ -546,8 +546,8 @@ function setFooter() {
         </div>
       </div>
     </div>
-    <div class="sp footer fcc z100 bgF9E8ED">
-      <div class="v fxc abs bot62">
+    <div class="sp footer fcc z100 w100p bgF9E8ED py60">
+      <div class="v fxc">
         <iframe class="w382 h227" src="${mapurl}" loading="lazy"></iframe>
         <img src="img/logo1.webp" class="w231 mt23" alt="logo" />
         <div class="f14 mt26 lh200">
